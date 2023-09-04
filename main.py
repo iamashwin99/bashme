f9cb333cbbfbc14ecc8fe6f54e1aee8fef7ae483
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-09-04 22:54:25 +0200

    move everything to main for now

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 logging.set_verbosity(logging.CRITICAL)
 
 def load_CodeLlama_llm():
+
+
+    pass
+
+def tokenise(prompt):
+    pass
+
+def main():
+
+    # load the model
     model_name_or_path = os.getenv("MPSD_CODE_LLAMA",None) or "../models/CodeLlama-7B-Instruct-GPTQ"
 
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
@@ -16,35 +26,25 @@ def load_CodeLlama_llm():
             use_safetensors=True,
             trust_remote_code=True,
             device="cuda:0",
-            use_triton=use_triton,
+            use_triton=False,
             quantize_config=None)
+    # set the prompt
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="prompt to generate code for")
+    args = parser.parse_args()
+    prompt = args.prompt
 
-    return model
-
-def tokenise(prompt):
+    # construct complete prompt
     prompt_template=f'''[INST] Write bash code to solve the following coding problem that obeys the constraints. Please wrap your code answer using ```. write single line bash codes as much as possible. Keep the explanation very short.:
     {prompt}
     [/INST]
     '''
     input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
     output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
-    return tokenizer.decode(output[0])
-
-def main():
-
-    # load the model
-    model = load_CodeLlama_llm()
-    # set the prompt
-    parser = argparse.ArgumentParser()
-    parser.add_argument("prompt", help="prompt to generate code for")
-    args = parser.parse_args()
-    prompt = args.prompt
-    code = tokenise(prompt)
-    print(code)
+    print( tokenizer.decode(output[0]).split('[/INST]')[-1] )
 
     # parse the results
     # print the results
-    pass
 
 
 if __name__ == '__main__':
