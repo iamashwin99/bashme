ac1349cb63db8990a709271b2049bd3d67f80441
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-10-19 23:48:14 +0200

    black

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from rich import print as rprint
 from rich.markdown import Markdown
 from rich.console import Console
 from rich.syntax import Syntax
+
 bashme_logo = """
 
          _nnnn_
@@ -30,46 +31,57 @@ _)      \.___.,|     .'
 
 logging.set_verbosity(logging.CRITICAL)
 
+
 def load_CodeLlama_llm():
-
-
     pass
+
 
 def tokenise(prompt):
     pass
+
 
 def main():
     # set the prompt
     parser = argparse.ArgumentParser()
     parser.add_argument("prompt", help="prompt to generate code for")
-    parser.add_argument("--explain", action="store_true", help="include explanation in output")
+    parser.add_argument(
+        "--explain", action="store_true", help="include explanation in output"
+    )
 
     args = parser.parse_args()
     prompt = args.prompt
     include_explanation = args.explain
 
     # load the model
-    model_name_or_path = os.getenv("MPSD_CODE_LLAMA",None) or "../models/CodeLlama-7B-Instruct-GPTQ"
+    model_name_or_path = (
+        os.getenv("MPSD_CODE_LLAMA", None) or "../models/CodeLlama-7B-Instruct-GPTQ"
+    )
 
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
 
-    model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
-            use_safetensors=True,
-            trust_remote_code=True,
-            device="cuda:0",
-            use_triton=False,
-            quantize_config=None)
+    model = AutoGPTQForCausalLM.from_quantized(
+        model_name_or_path,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device="cuda:0",
+        use_triton=False,
+        quantize_config=None,
+    )
 
-    detail_prompt = "Keep the explanation very short." if include_explanation else "No explanation required, write only code."
+    detail_prompt = (
+        "Keep the explanation very short."
+        if include_explanation
+        else "No explanation required, write only code."
+    )
 
     # construct complete prompt
-    prompt_template=f'''[INST] Write bash code to solve the following coding problem that obeys the constraints. Please wrap your code answer using ```. write single line bash codes as much as possible. If multiple steps are required split them. {detail_prompt} Use markdown syntax:
+    prompt_template = f"""[INST] Write bash code to solve the following coding problem that obeys the constraints. Please wrap your code answer using ```. write single line bash codes as much as possible. If multiple steps are required split them. {detail_prompt} Use markdown syntax:
     {prompt}
     [/INST]
-    '''
+    """
 
     # method 1
-    input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
+    input_ids = tokenizer(prompt_template, return_tensors="pt").input_ids.cuda()
     output = model.generate(inputs=input_ids, max_new_tokens=512)
     # output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
     raw_output = tokenizer.decode(output[0])
@@ -85,8 +97,8 @@ def main():
     #     repetition_penalty=1.15
     # )
     # raw_output = pipe(prompt_template)[0]['generated_text']
-    raw_output = raw_output.replace('</s>','')
-    raw_output = raw_output.split('[/INST]')[-1]
+    raw_output = raw_output.replace("</s>", "")
+    raw_output = raw_output.split("[/INST]")[-1]
     # rprint(raw_output)
     code = "$ " + raw_output.split("```bash")[1].split("```")[0].strip()
     explanation = raw_output.split("```bash")[1].split("```")[1]
@@ -98,11 +110,9 @@ def main():
     console.print(code_syntax)
     console.print(Markdown(explanation))
 
-
-
     # parse the results
     # print the results
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
