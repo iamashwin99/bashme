9bf322ec0637073308e813326eb72e12ba03ee5c
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-10-20 00:46:42 +0200

    add todo

--- lmod/bashme.lua
+++ lmod/bashme.lua
@@ -12,6 +12,7 @@ module = {
   function load_python(path)
     prepend_path("PATH", path .. "/bin")
     prepend_path("PYTHONPATH", path .. "/lib/python3.9/site-packages")
+    -- TODO Make this independant of system python
   end
 
 load_python("/opt_mpsd/linux-debian11/ashwins_playground/theaiplayground/bashme/bashmeenv")
